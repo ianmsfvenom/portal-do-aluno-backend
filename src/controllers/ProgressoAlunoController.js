8f58976fdd3b0b0eb36e2c6759e16f5716bec1ff
ianmsfvenom <iangamer296@gmail.com>
2025-04-29 19:59:04 -0300

    first commit

--- src/controllers/ProgressoAlunoController.js
+++ src/controllers/ProgressoAlunoController.js
@@ -1,6 +1,62 @@
+const prisma = require('../config/prisma');
+const HttpError = require('../errors/HttpError');
+
 module.exports = new class ProgressoAlunoController {
-    async create(req, res, next) {}
-    async delete(req, res, next) {}
-    async update(req, res, next) {}
-    async list(req, res) {}
+    async create(req, res, next) {
+        const { aluno_id, video_id, apostila_id, avaliacao_id, 
+            progresso_video, baixou_apostila, nota_avaliacao, 
+            data_ultima_interacao } = req.body;
+        try {
+            const [dia, mes, ano] = data_ultima_interacao.split('/');
+            const date = new Date(ano, mes - 1, dia);
+
+            const progresso_aluno = await prisma.progressoAluno.create({ data: { 
+                aluno_id, video_id, apostila_id, avaliacao_id, 
+                progresso_video, baixou_apostila, nota_avaliacao, 
+                data_ultima_interacao: date 
+            }});
+
+            return res.json(progresso_aluno);
+        } catch (error) {
+            next(new HttpError(500, 'Erro ao criar progresso do aluno'))
+        }
+    }
+    async delete(req, res, next) {
+        const { id } = req.body;
+        try {
+            const deleteProgressoAluno = await prisma.progressoAluno.delete({ where: { id } });
+            return res.json(deleteProgressoAluno);
+        } catch (error) {
+            next(new HttpError(500, 'Erro ao deletar progresso do aluno'));
+        }
+    }
+    async update(req, res, next) {
+        const { id, aluno_id, video_id, apostila_id, avaliacao_id, 
+            progresso_video, baixou_apostila, nota_avaliacao, 
+            data_ultima_interacao } = req.body;
+        try {
+            const [dia, mes, ano] = data_ultima_interacao.split('/');
+            const date = new Date(ano, mes - 1, dia);
+
+            const updateProgressoAluno = await prisma.progressoAluno.update({ where: { id }, data: { 
+                aluno_id, video_id, apostila_id, avaliacao_id, 
+                progresso_video, baixou_apostila, nota_avaliacao, 
+                data_ultima_interacao: date 
+            }});
+
+            return res.json(updateProgressoAluno);
+        } catch (error) {
+            next(new HttpError(500, 'Erro ao atualizar progresso do aluno'));
+        }
+    }
+    async list(req, res, next) {
+        try {
+            const progresso_alunos = await prisma.progressoAluno.findMany({ include: 
+                { apostila: true, avaliacoe: true, video: true, usuario: true } 
+            });
+            return res.json(progresso_alunos);
+        } catch (error) {
+            next(new HttpError(500, 'Erro ao listar progresso do aluno'));
+        }
+    }
 }
--- src/validators/ProgressoAlunoValidator.js
+++ src/validators/ProgressoAlunoValidator.js
@@ -1,6 +1,52 @@
+const HttpError = require('../errors/HttpError');
+
 module.exports = new class ProgressoAlunoValidator {
-    create(req, res, next) {}
-    delete(req, res, next) {}
-    update(req, res, next) {}
-    list(req, res, next) {}
+    create(req, res, next) {
+        const { aluno_id, video_id, apostila_id, avaliacao_id, 
+            progresso_video, baixou_apostila, nota_avaliacao, 
+            data_ultima_interacao } = req.body;
+
+        if (!aluno_id || !video_id || !apostila_id || !avaliacao_id || 
+            !progresso_video || !baixou_apostila || !nota_avaliacao || 
+            !data_ultima_interacao) return next(new HttpError(400, 'Todos os campos obrigatórios'));
+
+        if(isNaN(aluno_id) || isNaN(video_id) || isNaN(apostila_id) || 
+            isNaN(avaliacao_id) || isNaN(progresso_video) || 
+            isNaN(nota_avaliacao)) return next(new HttpError(400, 'Todos os campos devem ser numéricos'));
+
+        const [ dia, mes, ano ] = data_ultima_interacao.split('/');
+        if(!dia || !mes || !ano) return next(new HttpError(400, 'Data de assinatura obrigatória'));
+        const date = new Date(ano, mes - 1, dia);
+        if (isNaN(date.getTime())) return next(new HttpError(400, 'Data de assinatura inválida'));
+
+        next();
+    }
+    delete(req, res, next) {
+        const { id } = req.body;
+        if (!id) return next(new HttpError(400, 'Id obrigatório'));
+        if(isNaN(id)) return next(new HttpError(400, 'Id deve ser um número'));
+        next();
+    }
+    update(req, res, next) {
+        const { id, aluno_id, video_id, apostila_id, avaliacao_id, 
+            progresso_video, baixou_apostila, nota_avaliacao, 
+            data_ultima_interacao } = req.body;
+
+        if(!id) return next(new HttpError(400, 'Id é obrigatório'));
+        if(id && isNaN(id)) return next(new HttpError(400, 'Id deve ser um número'));
+        if(aluno_id && isNaN(aluno_id)) return next(new HttpError(400, 'Aluno deve ser um número'));
+        if(video_id && isNaN(video_id)) return next(new HttpError(400, 'Video deve ser um número'));
+        if(apostila_id && isNaN(apostila_id)) return next(new HttpError(400, 'Apostila deve ser um número'));
+        if(avaliacao_id && isNaN(avaliacao_id)) return next(new HttpError(400, 'Avaliação deve ser um número'));
+        if(progresso_video && isNaN(progresso_video)) return next(new HttpError(400, 'Progresso do video deve ser um número'));
+        if(nota_avaliacao && isNaN(nota_avaliacao)) return next(new HttpError(400, 'Nota da avaliação deve ser um número'));
+        if(data_ultima_interacao) {
+            const [dia, mes, ano] = data_ultima_interacao.split('/');
+            if(!dia || !mes || !ano) return next(new HttpError(400, 'Data de assinatura obrigatória'));
+            const date = new Date(ano, mes - 1, dia);
+            if(isNaN(date.getTime())) return next(new HttpError(400, 'Data de assinatura inválida'));
+        }
+        next();
+    }
+
 }
